5dbb3edf517fe77780dafe812e5cdd8511b23e9a
MysterAitch <MysterAitch@users.noreply.github.com>
2017-12-03 03:05:08 +0000

    Create install_neo4j.sh

---
+++ install_neo4j.sh
@@ -0,0 +1,12 @@
+    # Update System
+    sudo apt-get update
+    sudo apt-get -y upgrade
+    
+
+    # Install Neo4j
+    wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+    echo 'deb https://debian.neo4j.org/repo stable/' | sudo tee /etc/apt/sources.list.d/neo4j.list
+    sudo apt-get update
+
+    ## Until APOC is available for v3.2, stick with v3.1
+    sudo apt-get install -y neo4j=3.1.4
