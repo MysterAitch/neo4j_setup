e2ebec4ca82294c424220981b90ceb80d908d611
MysterAitch <MysterAitch@users.noreply.github.com>
2017-12-03 03:09:04 +0000

    Create update_neo4j_pass.sh

---
+++ update_neo4j_pass.sh
@@ -0,0 +1,3 @@
+
+    ## Note use of http (due to self-signed certs causing https connections to fail)
+    curl -H "Content-Type: application/json" -X POST -d '{"password":"neo4j-pass"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password
