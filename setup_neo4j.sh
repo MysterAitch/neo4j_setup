64a8ea4d26db56f8f2cc78ed60eb2536e938d83c
Roger Howell <howelrtc@aston.ac.uk>
2017-12-21 21:51:49 +0000

    moved config files etc into separate directory, to separate from custom-created/bespoke executable files

--- setup_neo4j.sh
+++ setup_neo4j.sh
@@ -1,3 +1,4 @@
-    sudo cp ./neo4j.conf /etc/neo4j/neo4j.conf
-    sudo cp ./apoc-3.1.3.6-all.jar /var/lib/neo4j/plugins/apoc-3.1.3.6-all.jar
+    sudo cp ./neo4j_files/neo4j.conf /etc/neo4j/neo4j.conf
+    sudo cp ./neo4j_files/apoc-3.1.3.6-all.jar /var/lib/neo4j/plugins/apoc-3.1.3.6-all.jar
+
     sudo service neo4j restart
