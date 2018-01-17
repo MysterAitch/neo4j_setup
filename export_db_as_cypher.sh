3aa859e9eef98e76fb53349d5da7937ccd0b3388
MysterAitch <MysterAitch@users.noreply.github.com>
2018-01-17 16:34:22 +0000

    Update export_db_as_cypher.sh

--- export_db_as_cypher.sh
+++ export_db_as_cypher.sh
@@ -1,3 +1,7 @@
+set -x # Activate debugging
+
 # Set path to tmp exported file
 filepath="/tmp/cyhper-exports-" + timestamp() + ".cypher"
 /usr/bin/neo4j-shell -c "CALL apoc.export.cypher.all(($filepath),{});"
+
+set +x # Deactivate debugging
