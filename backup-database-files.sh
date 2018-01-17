162eb9ac9b339411a3e3ee5532b1ac94099de9cb
MysterAitch <MysterAitch@users.noreply.github.com>
2018-01-17 15:34:17 +0000

    Create backup-database-files.sh

---
+++ backup-database-files.sh
@@ -0,0 +1,12 @@
+# Stop the service to enable changes
+#sudo service neo4j stop
+
+timestamp=$(date +%Y%m%d%H%M%S)
+foldername=neo4j-db-export"$timestamp"
+#filename=foldername
+
+mkdir ~/"$foldername"
+
+
+# Restart the service
+#sudo service neo4j start
