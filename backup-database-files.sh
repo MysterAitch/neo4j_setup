18c25d4f6adc5c371b4f678fa7a8eb3710cf5eb6
MysterAitch <MysterAitch@users.noreply.github.com>
2018-01-17 15:53:07 +0000

    updated export script to include milliseconds in export dir name

--- backup-database-files.sh
+++ backup-database-files.sh
@@ -1,8 +1,8 @@
 # Stop the service to enable changes
 #sudo service neo4j stop
 
-timestamp=$(date +%Y%m%d%H%M%S)
-foldername=neo4j-db-export"$timestamp"
+timestamp=$(date +%Y%m%d%H%M%S%3N)
+foldername=neo4j-db-export-"$timestamp"
 #filename=foldername
 
 mkdir ~/"$foldername"
