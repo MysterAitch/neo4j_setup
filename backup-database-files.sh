aa19ecae9c82cdd8f105ffc55ca96816ed60b28c
MysterAitch <MysterAitch@users.noreply.github.com>
2018-01-17 16:10:30 +0000

    Update backup-database-files.sh

--- backup-database-files.sh
+++ backup-database-files.sh
@@ -1,12 +1,17 @@
 # Stop the service to enable changes
 #sudo service neo4j stop
 
+# Define some variables to be used 
+# (notably/primarily so that the timestamp remains consistent between uses within this script)
 timestamp=$(date +%Y%m%d_%H%M%S%3N)
 foldername=neo4j-db-export-"$timestamp"
-#filename=foldername
+filename="$foldername".cypher
 
+# Make directory to be used for this export
 mkdir ~/"$foldername"
 
+# Copy (recursively) the neo4j data dir to the newly created folder
+cp -r /var/lib/neo4j/data/databases/graph.db ~/"$foldername"
 
 # Restart the service
 #sudo service neo4j start
