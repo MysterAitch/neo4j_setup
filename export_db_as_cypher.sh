85754f7dbd1a26536c8d942e98ba392b307d3b56
Roger Howell <howelrtc@aston.ac.uk>
2018-01-17 16:59:51 +0000

    copy out the exported cypher file

--- export_db_as_cypher.sh
+++ export_db_as_cypher.sh
@@ -1,12 +1,17 @@
 set -x # Activate debugging
 
-
 # Set path to tmp exported file
 timestamp=$(date +%Y%m%d_%H%M%S%3N)
-filepath=/tmp/cyhper-exports-"$timestamp".cypher
+filename=cypher-exports-"$timestamp".cypher
+filepath=/tmp/"$filename"
 echo "$filepath"
 
+# Execute command to export cypher
 /usr/bin/neo4j-shell -c "CALL apoc.export.cypher.all(\"$filepath\",{});"
+
+
+# Copy exported cypher file
+cp "$filepath" ~/cypher-exports/"$filename"
 
 
 set +x # Deactivate debugging
