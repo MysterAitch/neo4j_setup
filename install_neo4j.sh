ea17d3729f84e6d0bff64d855da92d205b90512d
MysterAitch <MysterAitch@users.noreply.github.com>
2018-01-31 17:55:56 +0000

    add debug flags

--- install_neo4j.sh
+++ install_neo4j.sh
@@ -1,3 +1,5 @@
+set -x # Activate debugging
+
     # Update System
     sudo apt-get update
     sudo apt-get -y upgrade
@@ -10,3 +12,5 @@
 
     ## Until APOC is available for v3.2, stick with v3.1
     sudo apt-get install -y neo4j=3.3.2
+
+set +x # Rectivate debugging
