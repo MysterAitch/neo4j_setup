da033c2022276e8335d1c04857bcddb14b7fdc08
MysterAitch <MysterAitch@users.noreply.github.com>
2018-01-31 18:07:01 +0000

    added debug flags

--- update_neo4j_pass.sh
+++ update_neo4j_pass.sh
@@ -1,3 +1,6 @@
+set -x # Activate debugging
 
     ## Note use of http (due to self-signed certs causing https connections to fail)
     curl -H "Content-Type: application/json" -X POST -d '{"password":"neo4j-pass"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password
+
+set +x # Rectivate debugging
