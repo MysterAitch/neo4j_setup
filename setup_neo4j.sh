8b5430ead27ae84671f7e9fb8f54b82963680297
MysterAitch <MysterAitch@users.noreply.github.com>
2018-01-31 18:12:31 +0000

    Update setup_neo4j.sh

--- setup_neo4j.sh
+++ setup_neo4j.sh
@@ -1,10 +1,10 @@
 set -x # Activate debugging
 
     # Overwrite config file with own (primarily to enable non-local access)
-    sudo cp ./neo4j_files/neo4j.conf /etc/neo4j/neo4j.conf
+    sudo cp ./neo4j.conf /etc/neo4j/neo4j.conf
     
     # Overwrite config file with own (primarily to set/override ulimit - permitted number of simultaneously open files)
-    cp ./neo4j_setup/neo4j_files/neo4j /etc/default/neo4j
+    cp ./neo4j_files/neo4j /etc/default/neo4j
     
     # Copy across the APOC plugin
     sudo cp ./neo4j_files/apoc-3.3.0.1-all.jar /var/lib/neo4j/plugins/apoc-3.3.0.1-all.jar
