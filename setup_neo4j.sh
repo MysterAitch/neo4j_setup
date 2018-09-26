4d16a778e3b4b5790c34f5b439f0c5e1829d3315
MysterAitch <MysterAitch@users.noreply.github.com>
2018-09-27 00:58:51 +0100

    Edit Neo4j setup script to enable the service (i.e. so that it starts on reboot)

--- setup_neo4j.sh
+++ setup_neo4j.sh
@@ -9,6 +9,9 @@ set -x # Activate debugging
     # Copy across the APOC plugin
     sudo cp ./neo4j_files/apoc-3.4.0.3-all.jar /var/lib/neo4j/plugins/apoc-3.4.0.3-all.jar
 
+    # Enable the Neo4j service (i.e. to start on reboot)
+    systemctl enable neo4j
+
     # Restart the service to enable changes
     sudo service neo4j restart
 
