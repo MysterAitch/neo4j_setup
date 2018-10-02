19c86a5cc21ef7a0f0d3465354ad00c6fc9118a2
MysterAitch <MysterAitch@users.noreply.github.com>
2018-10-02 11:45:29 +0100

    create export directory if not already exists

--- export_db_as_cypher.sh
+++ export_db_as_cypher.sh
@@ -4,15 +4,19 @@ set -x # Activate debugging
 timestamp=$(date +%Y%m%d_%H%M%S%3N)
 filename=cypher-exports-"$timestamp".cypher
 filepath=/tmp/"$filename"
-echo "$filepath"
+exportdir=~/cypher-exports/
+exportpath="$exportdir$filename"
+echo "temp file: `$filepath`"
+echo "export path: `$exportpath`"
 
 # Execute command to export cypher
 /usr/bin/neo4j-shell -c "CALL apoc.export.cypher.all(\"$filepath\",{});"
 
 
 # Copy exported cypher file
-cp "$filepath" ~/cypher-exports/"$filename"
+mkdir -p "$exportdir"
+#mkdir "$exportdir"
+cp "$filepath" "$exportpath"
 
 
 set +x # Deactivate debugging
-
