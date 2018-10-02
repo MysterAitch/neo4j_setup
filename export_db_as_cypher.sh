1dc17de03d311cecfe333c964f0c0aa6166595bf
MysterAitch <MysterAitch@users.noreply.github.com>
2018-10-02 12:06:52 +0100

    Added ability to also copy to vagrant dir if exists

--- export_db_as_cypher.sh
+++ export_db_as_cypher.sh
@@ -3,11 +3,16 @@ set -x # Activate debugging
 # Set path to tmp exported file
 timestamp=$(date +%Y%m%d_%H%M%S%3N)
 filename=cypher-exports-"$timestamp".cypher
-filepath=/tmp/"$filename"
-exportdir=~/cypher-exports/
+filepath="/tmp/$filename"
+exportdirname="cypher-exports"
+exportdir=~/"$exportdirname"/
 exportpath="$exportdir$filename"
-echo "temp file: `$filepath`"
-echo "export path: `$exportpath`"
+vagrantdir="/vagrant/"
+vagrantexportdir="$vagrantdir$exportdirname/"
+vagrantexportpath="$vagrantexportdir$filename"
+
+echo "temp file: $filepath"
+echo "export path: $exportpath"
 
 # Execute command to export cypher
 /usr/bin/neo4j-shell -c "CALL apoc.export.cypher.all(\"$filepath\",{});"
@@ -18,5 +23,10 @@ mkdir -p "$exportdir"
 #mkdir "$exportdir"
 cp "$filepath" "$exportpath"
 
+
+if [ -d "$vagrantdir" ]; then
+    mkdir -p "$vagrantexportdir"
+    cp "$filepath" "$vagrantexportpath"
+fi
 
 set +x # Deactivate debugging
